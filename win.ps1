8d176ac9d34b0ac968757dd4ed9adfbf75df6cb8
hYq7WD <84105354+hYq7WD@users.noreply.github.com>
2021-05-13 00:28:07 +0200

    Add files via upload

---
+++ win.ps1
@@ -0,0 +1,13 @@
+# Powershell
+Update-Help -Force
+
+# Windows update
+$u=Start-WUScan
+Install-WUUpdates -Updates $u
+
+Get-WindowsFeature -Name *RSAT* | Install-WindowsFeature
+
+# SSH
+Set-Service -Name sshd -StartupType 'Automatic'
+Get-NetFirewallRule -Name *ssh*
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
