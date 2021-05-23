0e2f3c07d412bc1029800f9a5153d95dd1ad8a9b
hYq7WD <84105354+hYq7WD@users.noreply.github.com>
2021-05-24 00:23:53 +0200

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -10,6 +10,7 @@ Get-WindowsFeature -Name *RSAT* | Install-WindowsFeature
 # SSH
 Get-WindowsCapability -Online -Name 'OpenSSH.Server*' | Add-WindowsCapability -Online
 Set-Service -Name sshd -StartupType 'Automatic'
+Start-Service sshd
 New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 
 Restart-Computer -Force
